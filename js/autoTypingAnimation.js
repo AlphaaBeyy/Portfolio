cb05d358a274f3b946dcd5311039942a0cdc6903
Alpha Bey <j3n34x@hotmail.com>
2024-01-21 16:38:48 +0300

    Update autoTypingAnimation.js

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<span>Merhaba, Ben Tamer Uğur</span> Amatör bir fotoğrafçıyım. aynı zamanda E-Ticaret, yazılım ve 3D modelleme ile uğraşıyorum. Umarım çalışma portföyümden keyif alırsınız ve bundan sonra birlikte harika bir şey yaratabiliriz!"
+        "<span>Merhaba, Ben Tamer Uğur</span> Amatör bir IT Yöneticisyim. aynı zamanda E-Ticaret, Yazılım ve 3D modelleme ile uğraşıyorum. Umarım çalışma portföyümden keyif alırsınız ve bundan sonra birlikte harika bir şey yaratabiliriz!"
       ],
       typeSpeed: 7,
       contentType: 'html'
